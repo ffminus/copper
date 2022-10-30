6ac6ed328d22bfb143fd6a1f3d583488b47bdd77
ffminus <ffminus@protonmail.com>
2022-10-30 19:59:07 +0100

    Enable strict linting rules for the whole crate

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// Prevent all unsafe code usage in crate
+#![forbid(unsafe_code)]
+// Enable stricter lints
+#![warn(
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_macro_rules,
+    unused_qualifications
+)]
+// Disable lints with false positives
+#![allow(clippy::option_if_let_else)]
