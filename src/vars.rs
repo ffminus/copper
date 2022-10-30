13b18505a22791f3e6cecffbeae0811809bec607
ffminus <ffminus@protonmail.com>
2022-10-30 19:59:09 +0100

    Encapsulate vars to track changes during propagation

--- src/vars.rs
+++ src/vars.rs
@@ -1,4 +1,5 @@
-use std::ops::Deref;
+use std::collections::HashSet;
+use std::ops::{Deref, Index};
 
 /// New-type wrapper to identify decision variables and expressions.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -23,4 +24,52 @@ impl Deref for VarId {
 pub struct Var {
     pub min: i32,
     pub max: i32,
+}
+
+/// Decision variable domains, encapsulated to track changes used to schedule propagators.
+#[derive(Clone, Debug)]
+pub struct Vars {
+    vars: Vec<Var>,
+
+    events: HashSet<VarId>,
+}
+
+impl Vars {
+    pub fn new(vars: &[Var]) -> Self {
+        Self {
+            vars: vars.to_vec(),
+            events: HashSet::new(),
+        }
+    }
+
+    pub fn set_min_and_max(&mut self, id: VarId, min: i32, max: i32) {
+        self.set_min(id, min);
+        self.set_max(id, max);
+    }
+
+    pub fn set_min(&mut self, id: VarId, min: i32) {
+        let var = &mut self.vars[*id];
+
+        if min > var.min {
+            var.min = min;
+            self.events.insert(id);
+        }
+    }
+
+    pub fn set_max(&mut self, id: VarId, max: i32) {
+        let var = &mut self.vars[*id];
+
+        if max < var.max {
+            var.max = max;
+            self.events.insert(id);
+        }
+    }
+}
+
+impl Index<VarId> for Vars {
+    type Output = Var;
+
+    fn index(&self, index: VarId) -> &Self::Output {
+        &self.vars[*index]
+    }
 }
