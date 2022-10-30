9689da13a5e9a97b8b155a8b404d11be5155a0ce
ffminus <ffminus@protonmail.com>
2022-10-30 19:59:17 +0100

    Access the solution value of a slice of variables

--- src/solution.rs
+++ src/solution.rs
@@ -10,6 +10,12 @@ impl Solution {
     pub(crate) const fn new(assignment: Vec<i32>) -> Self {
         Self(assignment)
     }
+
+    /// Access the solution value of a slice of variables.
+    #[must_use]
+    pub fn get_values(&self, xs: &[VarId]) -> Vec<i32> {
+        xs.iter().map(|id| self[*id]).collect()
+    }
 }
 
 impl Index<VarId> for Solution {
