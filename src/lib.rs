940ba34a2687e39fc3f6a42fee57874e5ece874a
ffminus <ffminus@protonmail.com>
2024-01-05 20:27:31 +0100

    Expose main entry point to declare variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 // Use `README.md` as documentation home page, to reduce duplication
 #![doc = include_str!("../README.md")]
 
+mod model;
 mod props;
 mod vars;
 
+pub use crate::model::Model;
 pub use crate::vars::VarId;
---
+++ src/model.rs
@@ -0,0 +1,33 @@
+use crate::props::Propagators;
+use crate::vars::{VarId, Vars};
+
+/// Library entry point used to declare decision variables and constraints, and configure search.
+#[derive(Debug, Default)]
+pub struct Model {
+    vars: Vars,
+    props: Propagators,
+}
+
+impl Model {
+    /// Create a new integer decision variable, with the provided domain bounds.
+    ///
+    /// Both lower and upper bounds are included in the domain.
+    /// This function will only create a decision variable if `min < max`.
+    pub fn new_var(&mut self, min: i32, max: i32) -> Option<VarId> {
+        if min < max {
+            Some(self.new_var_unchecked(min, max))
+        } else {
+            None
+        }
+    }
+
+    /// Create a new integer decision variable, with the provided domain bounds.
+    ///
+    /// Both lower and upper bounds are included in the domain.
+    ///
+    /// This function assumes that `min < max`.
+    fn new_var_unchecked(&mut self, min: i32, max: i32) -> VarId {
+        self.props.on_new_var();
+        self.vars.new_var_with_bounds(min, max)
+    }
+}
