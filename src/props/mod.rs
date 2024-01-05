d9be3e8e1e494df938fa3da4b12060deb982296e
ffminus <ffminus@protonmail.com>
2024-01-05 23:46:30 +0100

    Define propagation traits, `Prune` and `Propagate`

--- src/props/mod.rs
+++ src/props/mod.rs
@@ -1,3 +1,20 @@
+use dyn_clone::DynClone;
+
+use crate::vars::VarId;
+use crate::views::Context;
+
+/// Enforce a specific constraint by pruning domain of decision variables.
+pub trait Prune: core::fmt::Debug + DynClone {
+    /// Perform pruning based on variable domains and internal state.
+    fn prune(&mut self, ctx: &mut Context) -> Option<()>;
+}
+
+/// Isolate methods that prevent propagator from being used as a trait-object.
+pub trait Propagate: Prune + 'static {
+    /// List variables that schedule the propagator when their domain changes.
+    gen fn list_trigger_vars(&self) -> VarId;
+}
+
 /// Store internal state for each propagators, along with dependencies for when to schedule each.
 #[derive(Clone, Debug, Default)]
 pub struct Propagators {
