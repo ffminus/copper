d214a3feeae152bece0ec7af3d7f16d3a36cda92
ffminus <ffminus@protonmail.com>
2024-01-06 00:36:19 +0100

    Group data needed to perform search in `Space` struct

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,1 +1,11 @@
 mod agenda;
+
+use crate::props::Propagators;
+use crate::vars::Vars;
+
+/// Data required to perform search, copied on branch and discarded on failure.
+#[derive(Clone, Debug)]
+pub struct Space {
+    pub vars: Vars,
+    pub props: Propagators,
+}
